58acd5d75ea51e48abb1b2af10b66da7fdabca9a
MrMcSheeple <48631260+mrmcsheeple@users.noreply.github.com>
2021-05-15 23:46:26 -0400

    Created a commandline interface for the program

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python3
 from remove_levels import RemoveLevels
-import json
+import yaml
+import argparse
 
-level_remover = RemoveLevels("Adult Red Dragon")
-unmod_out = json.dumps(level_remover.monster, indent=4)
-out = json.dumps(level_remover.compose(), indent=4)
+parser = argparse.ArgumentParser()
+parser.add_argument('name', type=str, help='Name of the monster')
+parser.add_argument('--output-file', '-f', type=str, help='Optionally output to yaml file instead of console')
+
+args = parser.parse_args()
+
+try:
+    level_remover = RemoveLevels(args.name)
+    out = yaml.dump(level_remover.compose(), sort_keys=False)
+
+    if args.output_file:
+        with open(args.output_file, 'w') as f:
+            f.write(out)
+            f.close()
+    else:
+        print(out)
+
+except KeyError:
+    print("Monster Does Not Exist. Check Spelling.")
